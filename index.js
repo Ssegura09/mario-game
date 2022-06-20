00f892761fde78ea52efe764d7e61aa53ff48006
Stephanie Segura <Steph@Stephanies-MBP.attlocal.net>
2022-06-19 19:46:48 -0500

    Player movement done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const gravity = 0.5
+const gravity = 2.0
 
 class Player {
     constructor() {
@@ -29,6 +29,7 @@ class Player {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x 
         this.position.y += this.velocity.y 
 
         if(this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -38,11 +39,75 @@ class Player {
 }
 
 const player = new Player()
-player.update()
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
+
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+
+    if (keys.right.pressed){
+        player.velocity.x = 5
+    } else if (keys.left.pressed){
+        player.velocity.x = -5
+    } else player.velocity.x = 0
 }
 animate()
+
+// event listeners
+// window.addEventListener()
+addEventListener('keydown', ({keyCode}) => {
+    switch(keyCode) {
+        //a
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+        break
+        //s
+        case 83:
+            console.log('down')
+        break
+        //d
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+        break
+        //w
+        case 87:
+            console.log('up')
+            player.velocity.y -= 20
+        break
+    }
+})
+
+addEventListener('keyup', ({keyCode}) => {
+    switch(keyCode) {
+        //a
+        case 65:
+            console.log('left')
+            keys.left.pressed = false
+        break
+        //s
+        case 83:
+            console.log('down')
+        break
+        //d
+        case 68:
+            console.log('right')
+            keys.right.pressed = false
+        break
+        //w
+        case 87:
+            console.log('up')
+            player.velocity.y -= 20
+        break
+    }
+})
